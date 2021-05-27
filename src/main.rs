043c2b5da824504da51b5aa150f4854aaaccd52a
tcdk <458761603@qq.com>
2021-05-28 02:46:19 +0800

    fix: remove create::logger_config

--- src/main.rs
+++ src/main.rs
@@ -12,42 +12,6 @@ pub mod config;
 pub mod task;
 pub mod ws;
 
-fn logger_config() {
-    use log4rs::append::console::ConsoleAppender;
-    use log4rs::append::file::FileAppender;
-    use log4rs::config::{Appender, Config, Logger, Root};
-    use log4rs::encode::pattern::PatternEncoder;
-
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "[{d(%Y-%m-%d %H:%M:%S)}] [{l}] {M} - {m} {n}",
-        )))
-        .build();
-    let file_log = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "[{d(%Y-%m-%d %H:%M:%S)}] [{l}] {M} - {m} {n}",
-        )))
-        .build("log")
-        .unwrap();
-
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .appender(Appender::builder().build("file_log", Box::new(file_log)))
-        .logger(
-            Logger::builder()
-                .appender("file_log")
-                .build("bilili_danmuji_rs", log::LevelFilter::Info),
-        )
-        .build(
-            Root::builder()
-                .appender("stdout")
-                .build(log::LevelFilter::Info),
-        )
-        .unwrap();
-
-    log4rs::init_config(config).unwrap();
-}
-
 #[tokio::main]
 async fn main() {
     config::logger_config();
